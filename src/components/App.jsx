11ef31fd7d95a16f3c4ae53342667c29bf6039f1
Cocokringle <cocokringle.1@gmail.com>
2022-02-01 23:43:27 +0200

    добавляет первые два компонента

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+import Profile from 'components/Profile'
+import Statistics from './Statistics'
+import user from '../user'
+import data from '../data'
+
+export default function App (){
+    return <div>
+        <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+        />
+
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
     </div>
-  );
-};
+}
