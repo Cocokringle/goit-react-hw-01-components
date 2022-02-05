dc02b289f8b4ef6f56ee1bccdcd0315bfbe4fe4e
Cocokringle <cocokringle.1@gmail.com>
2022-02-05 18:10:36 +0200

    упорядочивает файлы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import Profile from 'components/Profile/Profile'
 import Statistics from './Statistics/Statistics'
 import FriendList from './FriendList/FriendList'
 import TransactionHistory from './TransactionHistory/TransactionHistory'
-import user from '../user'
-import data from '../data'
-import friends from '../friends'
-import transactions from '../transactions'
+import user from '../components/data/user'
+import data from '../components/data/data'
+import friends from '../components/data/friends'
+import transactions from '../components/data/transactions'
 
 export default function App (){
     return <div>
